601b72a3710f1381f0301fecbbe6e98f925f0e79
ian.dover <ian.dover@skunova.com>
2023-02-01 17:07:27 +0000

    improved web scraper

--- scrape_pwcc.py
+++ scrape_pwcc.py
@@ -1,10 +1,11 @@
 import time
 from bs4 import BeautifulSoup
 from selenium import webdriver
-from selenium.webdriver.common.by import By
+import numpy as np
 
 def get_hrefs(
-    url : str
+    url : str,
+    sleep_time : int = 10
 ) -> list:
     '''
     get_hrefs():
@@ -14,35 +15,58 @@ def get_hrefs(
     driver = webdriver.Chrome()
     driver.get(url)
 
-    time.sleep(10)
+    time.sleep(sleep_time)
 
-    links = driver.find_elements("xpath", "//a[@data-testid='item-title']")
+    links = driver.find_elements('xpath', "//a[@data-testid='item-title']")
     hrefs = []
     for link in links:
         hrefs.append(link.get_attribute('href'))
 
     return hrefs
 
-def get_all_img_urls(
-    base_url : str
+def get_all_img_hrefs(
+    base_url : str,
+    sample_limit : int = 125,
+    sleep_time : int = 120
 ) -> list:
     '''
-    get_all_img_urls():
-        Given a base URL, this function returns a list of URLs for all `img` elements that have an `alt` attribute
-        ending with 'Back' in the HTML of all pages at the given URL and its paginated versions.
+    get_all_img_hrefs():
+        Given a base URL, paginate and return a list of URLs
+        for all `a` elements that have an `data-testid` attribute
+        equal to 'item-title' in the HTML of the page at the given URL.
     '''
     urls = []
     page = 1
     while True:
         page_url = fr'{base_url}&page={str(page)}'
-        page_img_urls = get_hrefs(page_url)
-        if not page_img_urls:
+
+        time.sleep(sleep_time)
+
+        page_img_hrefs = get_hrefs(page_url)
+
+        if not page_img_hrefs:
             break
-        urls += page_img_urls
+
+        urls += page_img_hrefs
+
+        if len(urls) > sample_limit:
+            break
+
         page += 1
     return urls
 
 if __name__ == '__main__':
-    base_url = 'https://www.pwccmarketplace.com/weekly-auction?q=pokemon+psa+6'
-    result = get_all_img_urls(base_url)
-    print(result)
+    psa_values = np.arange(2, 5, 1)
+
+    FILENAME = 'pwcc_card_pages.csv'
+    file = open(FILENAME, 'w')
+    file.write('IMAGE_LOCATION|PSA_VALUE' + '\n')
+
+    for psa_value in psa_values:
+        base_url = fr'https://www.pwccmarketplace.com/weekly-auction?q=pokemon+psa+{psa_value}'
+        result = get_all_img_hrefs(base_url)
+
+        for item in result:
+            file.write(fr'{item}|{psa_value}' + '\n')
+
+    file.close()
